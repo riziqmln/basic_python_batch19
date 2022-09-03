ba434df0ef7535065a1a2161135baf7a6c1c7a84
riziqmln <riziqanam@gmail.com>
2022-09-03 15:31:19 +0800

    Pertemuan pertama Basic Python

---
+++ variable.py
@@ -0,0 +1,41 @@
+x = 5
+y = 3.3
+xy = x*y
+v = 3.5
+h = 'Indonesia'
+zz = 'Indonesia AI'
+zzz = "Indonesia AI"
+is_true = True
+x_string = "5"
+
+x_float = float(x)
+y_int = int(y)
+x_int = int(x_string)
+n = 2*6/3
+
+print(n, type(n))
+print(x_float, type(x_float))
+print(y_int, type(y_int))
+print(x_int, type(x_int))
+
+
+print("Hasil dari 9 modulus 2 adalah", 9%2)
+print("Hasil dari 9 pangkat 2 adalah", 9**2)
+print(type(is_true))
+print(type(zz))
+print(type(x))
+print(type(y))
+
+print(zz)
+print(zzz)
+
+z = "Rony"
+
+# Memprint hasil variable di atas
+#Untuk menjummlahkan
+print(x+y)
+print(z)
+#Untuk mengalikan
+print(xy)
+print("Hasil Dari", x, "dikalikan dengan", y, "sama dengan", xy)
+
